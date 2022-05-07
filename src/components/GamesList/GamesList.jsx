993a6c9afe1612c71f82d964fc9949cb9c14db8e
Esdras Silva <esdraslimasilva83@gmail.com>
2022-05-07 09:55:38 -0300

    Implement pagination

--- src/components/GamesList/GamesList.jsx
+++ src/components/GamesList/GamesList.jsx
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { createRef, useState } from "react";
 import SingleGame from "../singleGame/SingleGame";
+import { useSelector } from "react-redux";
+import { selectGames } from "../../redux/gamesSlice";
 
 function GamesList() {
+  const games = useSelector(selectGames);
+  const [currentPage, setCurrentPage] = useState(1);
+  const maxPage = Math.ceil(games.length / 10);
+  const gamesListPage = games.slice(currentPage * 10 - 10, currentPage * 10);
+  console.log(games, currentPage, maxPage);
+
+  if (games.length !== 0) {
+    return (
+      <section>
+        {gamesListPage.map((game) => (
+          <SingleGame key={game.id} name={game.title} />
+        ))}
+        <div id="pagination" className="text-gray-50">
+          {currentPage > 1 ? (
+            <button
+              className="bg-blue-700 px-2 rounded-l-lg font-mono font-bold hover:bg-gray-50 hover:text-blue-700 hover:cursor-pointer mx-1 transition-all text-sm"
+              onClick={() => setCurrentPage((prevState) => prevState - 1)}
+            >
+              Back
+            </button>
+          ) : (
+            ""
+          )}
+          {currentPage < maxPage ? (
+            <button
+              className="bg-blue-700 px-2 rounded-r-lg font-mono font-bold hover:bg-gray-50 hover:text-blue-700 hover:cursor-pointer mx-1 transition-all text-sm"
+              onClick={() => setCurrentPage((prevState) => prevState + 1)}
+            >
+              Next
+            </button>
+          ) : (
+            ""
+          )}
+        </div>
+      </section>
+    );
+  }
+
   return (
-    <>
-      <SingleGame />
-    </>
+    <h2 className="text-gray-50">Try to choose one category on the menu</h2>
   );
 }
 
--- src/components/singleGame/SingleGame.jsx
+++ src/components/singleGame/SingleGame.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { selectGames } from "../../redux/gamesSlice";
-function SingleGame() {
-  const games = useSelector(selectGames);
-  console.log(games);
 
-  if (games) {
-    return <h2 className="bg-slate-100">Empty</h2>;
-  }
-  return <div className="bg-slate-100">fullField</div>;
+function SingleGame({ name }) {
+  return (
+    <>
+      <div>
+        <h2 className="text-white">{name}</h2>
+      </div>
+    </>
+  );
 }
 
 export default SingleGame;
